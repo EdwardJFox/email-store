c175fda9ef605576d1781258f9bde0db05b0bfce
Edward Fox <edward.fox@epigenesys.org.uk>
2019-11-26 13:18:57 +0000

    Switched to the more recent Webdrivers gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,19 +33,35 @@ Capybara.register_driver :ie_7_driver do |app|
   Capybara::RackTest::Driver.new(app, :headers => { 'HTTP_USER_AGENT' => 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)' })
 end
 
-require "selenium/webdriver"
+require 'webdrivers'
+
+Webdrivers.install_dir = Rails.root.join('vendor', 'webdrivers')
+Webdrivers.cache_time = 86_400
 
 Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w(headless disable-gpu window-size=1920,1080 disable-xss-auditor) }
-  )
-
-  Capybara::Selenium::Driver.new app,
-    browser: :chrome,
-    desired_capabilities: capabilities
+  if ENV['SELENIUM_HOST']
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :remote,
+      url: "http://#{ENV['SELENIUM_HOST']}:#{ENV['SELENIUM_PORT']}/wd/hub",
+      desired_capabilities: :chrome
+    )
+  else
+    chrome_options = Selenium::WebDriver::Chrome::Options.new
+    chrome_options.add_argument('--headless') unless ENV['SHOW_CHROME']
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-infobars')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--disable-popup-blocking')
+    chrome_options.add_argument('--window-size=1920,1080')
+    
+    Capybara::Selenium::Driver.new app, browser: :chrome, options: chrome_options
+  end
 end
-
 Capybara.javascript_driver = :headless_chrome
+
 
 require 'webmock/rspec'
 WebMock.disable_net_connect!(allow_localhost: true)
